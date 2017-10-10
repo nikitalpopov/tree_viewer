6c428b41a4af8b46fded84f46435b4745b3dbce7
nikitalpopov <tapochek97@ya.ru>
2017-10-10 20:00:49 +0300

    update

--- app/tree-viewer/tree-viewer.component.js
+++ app/tree-viewer/tree-viewer.component.js
@@ -2,7 +2,7 @@ angular
     .module('treeViewerApp')
     .component('treeViewer', {
             templateUrl: 'tree-viewer/tree-viewer.template.html',
-            controller: function TreeViewerController($scope, $http) {
+            controller: function TreeViewerController($http) {
                 var self = this;
                 var url = 'https://rawgit.com/nikitalpopov/abd9317b4853e8d2caee432855e0d0eb/raw/5d43765ba95d6ab346ce2fb5b4ab2958f52af7fc/tree.json';
 
