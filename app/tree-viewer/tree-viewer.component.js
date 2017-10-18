ca0e6402be6ce8f51d6c268d4147d0aac40d9cd1
nikitalpopov <tapochek97@ya.ru>
2017-10-18 15:44:28 +0300

    update

--- app/tree-viewer/tree-viewer.component.js
+++ app/tree-viewer/tree-viewer.component.js
@@ -4,7 +4,6 @@ angular
             templateUrl: 'tree-viewer/tree-viewer.template.html',
             controller: function TreeViewerController($http) {
                 var self = this;
-                // var url = 'https://rawgit.com/nikitalpopov/abd9317b4853e8d2caee432855e0d0eb/raw/79dc0d6f64390e817cbe3c7990ccf5519e9570ff/tree.json';
                 var url = '/public/tree.json';
 
                 $http
